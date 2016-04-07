5d7bf1ba442039c391fea11b508be5730aa8ce8f
Brandon Williams <mbw234@gmail.com>
2016-04-07 10:58:21 -0400

    Fix withLatestFrom and takeWhen interfaces.

--- ReactiveExtensions/operators/TakeWhen.swift
+++ ReactiveExtensions/operators/TakeWhen.swift
@@ -19,13 +19,13 @@ public extension SignalProducerType {
 
   /// Emits the latest value of `self` when `other` emits.
   @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public func takeWhen <U> (other: SignalProducer<U, Error>) -> SignalProducer<Value, Error> {
-    return lift(Signal.takeWhen)(other)
+  public func takeWhen <U> (other: Signal<U, Error>) -> Signal<Value, Error> {
+    return other.withLatestFrom(self.producer).map { $0.1 }
   }
 
   /// Emits the latest value of `self` when `other` emits.
   @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public func takeWhen <U> (other: Signal<U, Error>) -> SignalProducer<Value, Error> {
-    return lift { $0.takeWhen(other) }
+  public func takePairWhen <U> (other: Signal<U, Error>) -> Signal<(Value, U), Error> {
+    return other.withLatestFrom(self.producer).map { ($0.1, $0.0) }
   }
 }
--- ReactiveExtensions/operators/WithLatestFrom.swift
+++ ReactiveExtensions/operators/WithLatestFrom.swift
@@ -2,7 +2,14 @@ import ReactiveCocoa
 
 public extension SignalType {
 
-  /// Combines values from two signals only when `self` emits.
+  /**
+   Transforms the signal into one that emits the most recent values of `self` and `other` only when `self`
+   emits.
+
+   - parameter other: A signal.
+
+   - returns: A new signal.
+   */
   @warn_unused_result(message="Did you forget to call `observe` on the signal?")
   public func withLatestFrom <U, OtherError: ErrorType> (other: Signal<U, OtherError>) -> Signal<(Value, U), OtherError> {
 
@@ -37,7 +44,7 @@ public extension SignalType {
           }
           lock.unlock()
         case .Failed, .Completed, .Interrupted:
-          // don't fail, complete or interrupt when the sample does
+          // don't fail, complete or interrupt when the other does
           break;
         }
       }
@@ -45,13 +52,55 @@ public extension SignalType {
       return disposable
     }
   }
-}
 
-public extension SignalProducerType {
+  /**
+   Transforms the signal into one that emits the most recent values of `self` and `other` only when `self`
+   emits.
 
-  /// Combines values from two signals only when `other` emits.
-  @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public func withLatestFrom <U> (other: SignalProducer<U, Error>) -> SignalProducer<(Value, U), Error> {
-    return lift(Signal.combineLatestWith)(other)
+   - parameter other: A producer.
+
+   - returns: A new signal.
+   */
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public func withLatestFrom <U, OtherError: ErrorType> (other: SignalProducer<U, OtherError>) -> Signal<(Value, U), OtherError> {
+
+    return Signal { observer in
+      let lock = NSLock()
+      lock.name = "org.reactivecocoa.ReactiveCocoa.withLatestFrom"
+
+      let disposable = CompositeDisposable()
+      var latestValue: U? = nil
+
+      disposable += other.start { event in
+        switch event {
+        case let .Next(value):
+          lock.lock()
+          latestValue = value
+          lock.unlock()
+        case let .Failed(error):
+          observer.sendFailed(error)
+        case .Completed:
+          observer.sendCompleted()
+        case .Interrupted:
+          observer.sendInterrupted()
+        }
+      }
+
+      disposable += self.signal.observe { event in
+        switch event {
+        case let .Next(value):
+          lock.lock()
+          if let latestValue = latestValue {
+            observer.sendNext((value, latestValue))
+          }
+          lock.unlock()
+        case .Failed, .Completed, .Interrupted:
+          // don't fail, complete or interrupt when the other does
+          break;
+        }
+      }
+
+      return disposable
+    }
   }
 }
