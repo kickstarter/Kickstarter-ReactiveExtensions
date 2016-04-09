c6fd7de18a5ee0548703e84cb635af7a822db4d3
Brandon Williams <mbw234@gmail.com>
2016-04-09 10:11:41 -0400

    Better to use a MutablePropertyType where possible.

--- ReactiveExtensions/operators/DemoteErrors.swift
+++ ReactiveExtensions/operators/DemoteErrors.swift
@@ -2,19 +2,32 @@ import ReactiveCocoa
 import Result
 
 public extension SignalType {
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public func demoteErrors(replaceErrorWith value: Value? = nil) -> Signal<Value, NoError> {
+
+    return self.signal
+      .flatMapError { error in
+        if let value = value {
+          return SignalProducer(value: value)
+        }
+        return SignalProducer.empty
+    }
+  }
+
   /**
    Demotes the `Error` of this signal to `NoError`, thus preventing it from ever erroring. Essentially the
    inverse of `promoteErrors`.
 
    - parameter value:  An optional value that will be played in place of the error.
-   - parameter errors: An optional property to send errors to.
+   - parameter errors: A mutable property to send errors to.
 
    - returns: A new signal that will never error.
    */
   @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-  public func demoteErrors(
+  public func demoteErrors <MP: MutablePropertyType where MP.Value == Error?> (
     replaceErrorWith value: Value? = nil,
-                     pipeErrorsTo errors: MutableProperty<Error?>? = nil) -> Signal<Value, NoError> {
+    pipeErrorsTo errors: MP?) -> Signal<Value, NoError> {
 
     return self.signal
       .on(failed: { error in
@@ -37,15 +50,27 @@ public extension SignalProducerType {
    inverse of `promoteErrors`.
 
    - parameter value:  An optional value that will be played in place of the error.
-   - parameter errors: An optional property to send errors to.
 
    - returns: A new producer that will never error.
    */
   @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public func demoteErrors(
-    replaceErrorWith value: Value? = nil,
-                     pipeErrorsTo errors: MutableProperty<Error?>? = nil) -> SignalProducer<Value, NoError> {
+  public func demoteErrors(replaceErrorWith value: Value? = nil) -> SignalProducer<Value, NoError> {
+    return self.lift { $0.demoteErrors(replaceErrorWith: value) }
+  }
 
+  /**
+   Demotes the `Error` of the producer to `NoError`, thus preventing it from ever erroring. Essentially the
+   inverse of `promoteErrors`.
+
+   - parameter value:  An optional value that will be played in place of the error.
+   - parameter errors: A mutable property to send errors to.
+
+   - returns: A new producer that will never error.
+   */
+  @warn_unused_result(message="Did you forget to call `start` on the producer?")
+  public func demoteErrors <MP: MutablePropertyType where MP.Value == Error?> (
+    replaceErrorWith value: Value? = nil,
+    pipeErrorsTo errors: MP?) -> SignalProducer<Value, NoError> {
     return self.lift { $0.demoteErrors(replaceErrorWith: value, pipeErrorsTo: errors) }
   }
 }
--- ReactiveExtensionsTests/operators/DemoteErrorsTests.swift
+++ ReactiveExtensionsTests/operators/DemoteErrorsTests.swift
@@ -23,7 +23,50 @@ final class DemoteErrorTests: XCTestCase {
     test.assertDidComplete()
   }
 
-  func testDemoteErrors_Signal_WithArguements() {
+  func testDemoteErrors_Signal_WithReplacementValue() {
+    let (signal, observer) = Signal<Int, SomeError>.pipe()
+    let testSignal = signal.demoteErrors(replaceErrorWith: 99)
+
+    let test = TestObserver<Int, NoError>()
+    testSignal.observe(test.observer)
+
+    observer.sendNext(1)
+    observer.sendNext(2)
+    observer.sendNext(3)
+    observer.sendFailed(SomeError())
+
+    test.assertValues([1, 2, 3, 99])
+    test.assertDidNotFail()
+    test.assertDidComplete()
+  }
+
+  func testDemoteErrors_Signal_WithErrorsProperty() {
+    let (signal, observer) = Signal<Int, SomeError>.pipe()
+    let errors = MutableProperty<SomeError?>(nil)
+    let testSignal = signal.demoteErrors(pipeErrorsTo: errors)
+
+    let test = TestObserver<Int, NoError>()
+    testSignal.observe(test.observer)
+
+    let errorTest = TestObserver<SomeError?, NoError>()
+    errors.signal.observe(errorTest.observer)
+
+    observer.sendNext(1)
+    observer.sendNext(2)
+    observer.sendNext(3)
+    observer.sendFailed(SomeError())
+
+    test.assertValues([1, 2, 3])
+    test.assertDidNotFail()
+    test.assertDidComplete()
+
+    errorTest.assertValueCount(1)
+    errorTest.assertDidNotComplete()
+    errorTest.assertDidNotFail()
+  }
+
+
+  func testDemoteErrors_Signal_WithReplacementAndErrorsProperty() {
     let (signal, observer) = Signal<Int, SomeError>.pipe()
     let errors = MutableProperty<SomeError?>(nil)
     let testSignal = signal.demoteErrors(replaceErrorWith: 99, pipeErrorsTo: errors)
@@ -65,7 +108,50 @@ final class DemoteErrorTests: XCTestCase {
     test.assertDidComplete()
   }
 
-  func testDemoteErrors_Producer_WithArguements() {
+  func testDemoteErrors_Producer_WithReplacementValue() {
+    let (producer, observer) = SignalProducer<Int, SomeError>.buffer(0)
+    let testSignal = producer.demoteErrors(replaceErrorWith: 99)
+
+    let test = TestObserver<Int, NoError>()
+    testSignal.start(test.observer)
+
+    observer.sendNext(1)
+    observer.sendNext(2)
+    observer.sendNext(3)
+    observer.sendFailed(SomeError())
+
+    test.assertValues([1, 2, 3, 99])
+    test.assertDidNotFail()
+    test.assertDidComplete()
+  }
+
+  func testDemoteErrors_Producer_WithErrorsProperty() {
+    let (producer, observer) = SignalProducer<Int, SomeError>.buffer(0)
+    let errors = MutableProperty<SomeError?>(nil)
+    let testSignal = producer.demoteErrors(pipeErrorsTo: errors)
+
+    let test = TestObserver<Int, NoError>()
+    testSignal.start(test.observer)
+
+    let errorTest = TestObserver<SomeError?, NoError>()
+    errors.signal.observe(errorTest.observer)
+
+    observer.sendNext(1)
+    observer.sendNext(2)
+    observer.sendNext(3)
+    observer.sendFailed(SomeError())
+
+    test.assertValues([1, 2, 3])
+    test.assertDidNotFail()
+    test.assertDidComplete()
+
+    errorTest.assertValueCount(1)
+    errorTest.assertDidNotComplete()
+    errorTest.assertDidNotFail()
+  }
+
+
+  func testDemoteErrors_Producer_WithReplacementAndErrorsProperty() {
     let (producer, observer) = SignalProducer<Int, SomeError>.buffer(0)
     let errors = MutableProperty<SomeError?>(nil)
     let testSignal = producer.demoteErrors(replaceErrorWith: 99, pipeErrorsTo: errors)
@@ -89,4 +175,5 @@ final class DemoteErrorTests: XCTestCase {
     errorTest.assertDidNotComplete()
     errorTest.assertDidNotFail()
   }
+
 }
