15eb3ea7a7283c6a94ec8f485b75c389add4883c
Brandon Williams <mbw234@gmail.com>
2016-04-20 14:15:25 -0400

    Added a RAC test helper for arrays of optionals of equatables. (#15)

--- ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
+++ ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
@@ -1,4 +1,5 @@
 import XCTest
+import ReactiveCocoa
 
 // Assert equality between two doubly nested arrays of equatables.
 internal func XCTAssertEqual<T: Equatable>(@autoclosure expression1: () -> [[T]],
@@ -13,4 +14,24 @@ internal func XCTAssertEqual<T: Equatable>(@autoclosure expression1: () -> [[T]]
     zip(lhs, rhs).forEach { xs, ys in
       XCTAssertEqual(xs, ys, "Expected \(lhs), but found \(rhs): \(message)", file: file, line: line)
     }
+}
+
+// Assert equality between arrays of optionals of equatables.
+internal func XCTAssertEqual <T: ReactiveCocoa.OptionalType where T.Wrapped: Equatable>
+  (expression1: [T], _ expression2: [T], _ message: String = "",
+   file: StaticString = #file, line: UInt = #line) {
+
+  XCTAssertEqual(
+    expression1.count, expression2.count,
+    "Expected \(expression1.count) elements, but found \(expression2.count).",
+    file: file, line: line
+  )
+
+  zip(expression1, expression2).forEach { xs, ys in
+    XCTAssertEqual(
+      xs.optional, ys.optional,
+      "Expected \(expression1), but found \(expression2): \(message)",
+      file: file, line: line
+    )
+  }
 }
