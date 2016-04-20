ca90b7f8974ce087b32cf28292094132d86eca40
Brandon Williams <mbw234@gmail.com>
2016-04-20 08:27:42 -0400

    Added a RAC test helper for arrays of optionals of equatables.

--- ReactiveExtensionsTests/TestHelpers/TestObserver.swift
+++ ReactiveExtensionsTests/TestHelpers/TestObserver.swift
@@ -143,6 +143,29 @@ extension TestObserver where Value: Equatable {
   }
 }
 
+extension TestObserver where Value: ReactiveCocoa.OptionalType, Value.Wrapped: Equatable {
+
+  internal func assertValue(value: Value, _ message: String? = nil,
+                            file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(1, self.values.count, "A single item should have been emitted.", file: file, line: line)
+    XCTAssertEqual(value.optional, self.lastValue!.optional,
+                   message ?? "A single value of \(value) should have been emitted",
+                   file: file, line: line)
+  }
+
+  internal func assertLastValue(value: Value, _ message: String? = nil,
+                                file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(value.optional, self.lastValue!.optional,
+                   message ?? "Last emitted value is equal to \(value).",
+                   file: file, line: line)
+  }
+
+  internal func assertValues(values: [Value], _ message: String = "",
+                             file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(values, self.values, message, file: file, line: line)
+  }
+}
+
 extension TestObserver where Value: SequenceType, Value.Generator.Element: Equatable {
 
   internal func assertValue(value: Value, _ message: String? = nil,
