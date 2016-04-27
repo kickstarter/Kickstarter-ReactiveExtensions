283b0587386d0b60e12b3871e57dfc5f988f9a3d
Brandon Williams <mbw234@gmail.com>
2016-04-27 14:29:25 -0400

    Static concat method (#17)
    * Added static concat method.
    
    * Added concat for producers.

---
+++ ReactiveExtensions/operators/Concat.swift
@@ -0,0 +1,68 @@
+import ReactiveCocoa
+
+extension SignalType {
+
+  /**
+   Concats a sequence of signals into a single signal.
+
+   - parameter signals: A sequence of signals.
+
+   - returns: A concatenated signal.
+   */
+  public static func concat<S: SequenceType where S.Generator.Element == Signal<Value, Error>>
+    (signals: S) -> Signal<Value, Error> {
+
+      let producer = SignalProducer<Signal<Value, Error>, Error>(values: signals)
+      var result: Signal<Value, Error>!
+      producer.startWithSignal { (signal, _) in
+        result = signal.flatten(.Concat)
+      }
+      return result
+  }
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public static func concat(s1: Signal<Value, Error>,
+                            _ s2: Signal<Value, Error>) -> Signal<Value, Error> {
+
+    return Signal.concat([s1, s2])
+  }
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public static func concat(s1: Signal<Value, Error>,
+                            _ s2: Signal<Value, Error>,
+                            _ s3: Signal<Value, Error>) -> Signal<Value, Error> {
+
+    return Signal.concat([s1, s2, s3])
+  }
+}
+
+extension SignalProducerType {
+
+  /**
+   Concats a sequence of producers into a single producer.
+
+   - parameter producers: A sequence of producers.
+
+   - returns: A concatenated producer.
+   */
+  public static func concat<S: SequenceType where S.Generator.Element == SignalProducer<Value, Error>>
+    (producers: S) -> SignalProducer<Value, Error> {
+
+    return SignalProducer(values: producers).flatten(.Concat)
+  }
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public static func concat(s1: SignalProducer<Value, Error>,
+                            _ s2: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
+
+    return SignalProducer.concat([s1, s2])
+  }
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public static func concat(s1: SignalProducer<Value, Error>,
+                            _ s2: SignalProducer<Value, Error>,
+                            _ s3: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
+
+    return SignalProducer.concat([s1, s2, s3])
+  }
+}
