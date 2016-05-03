52102a12279a3b17874fe25eb7926a52f656ff9e
Brandon Williams <mbw234@gmail.com>
2016-05-03 11:03:34 -0400

    Rac extensions for font and textColor. (#19)
    * Rac extensions for font and textColor.
    
    * Gotta use separate association ints for these.

--- ReactiveExtensions/UIKit/UILabel.swift
+++ ReactiveExtensions/UIKit/UILabel.swift
@@ -4,14 +4,50 @@ import UIKit
 
 private enum Associations {
   private static var text = 0
+  private static var font = 1
+  private static var textColor = 2
 }
 
 public extension Rac where View: UILabel {
   public var text: Signal<String, NoError> {
     nonmutating set {
-      let prop: MutableProperty<String> = lazyMutableProperty(view, key: &Associations.text,
-        setter: { [weak view] in view?.text = $0 ?? "" },
+      let prop: MutableProperty<String> = lazyMutableProperty(
+        view,
+        key: &Associations.text,
+        setter: { [weak view] in view?.text = $0 },
         getter: { [weak view] in view?.text ?? "" })
+
+      prop <~ newValue.observeForUI()
+    }
+
+    get {
+      return .empty
+    }
+  }
+
+  public var font: Signal<UIFont, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<UIFont> = lazyMutableProperty(
+        view,
+        key: &Associations.font,
+        setter: { [weak view] in view?.font = $0 },
+        getter: { [weak view] in view?.font ?? .systemFontOfSize(12.0) })
+
+      prop <~ newValue.observeForUI()
+    }
+
+    get {
+      return .empty
+    }
+  }
+
+  public var textColor: Signal<UIColor, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<UIColor> = lazyMutableProperty(
+        view,
+        key: &Associations.textColor,
+        setter: { [weak view] in view?.textColor = $0 },
+        getter: { [weak view] in view?.textColor ?? .blackColor() })
 
       prop <~ newValue.observeForUI()
     }
