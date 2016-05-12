05e9abb0a11eec441d04b1f2f817624d2683d3f6
Brandon Williams <mbw234@gmail.com>
2016-05-12 16:58:41 -0400

    Reactive bindings for UIRefreshControl.

---
+++ ReactiveExtensions/UIKit/UIRefreshControl.swift
@@ -0,0 +1,25 @@
+import ReactiveCocoa
+import Result
+import UIKit
+
+private enum Associations {
+  private static var isRefreshing = 0
+}
+
+public extension Rac where Object: UIRefreshControl {
+  public var refreshing: Signal<Bool, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<Bool> = lazyMutableProperty(
+        object,
+        key: &Associations.isRefreshing,
+        setter: { [weak object] in $0 ? object?.beginRefreshing() : object?.endRefreshing() },
+        getter: { [weak object] in object?.refreshing ?? false })
+
+      prop <~ newValue.observeForUI()
+    }
+
+    get {
+      return .empty
+    }
+  }
+}
