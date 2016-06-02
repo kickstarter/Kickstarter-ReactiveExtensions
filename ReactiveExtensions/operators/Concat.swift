224164c45d2e652811a2676744c229afed7cee32
Brandon Williams <mbw234@gmail.com>
2016-06-01 23:19:48 -0400

    Removed a bunch of unneeded operators.

--- ReactiveExtensions/operators/Concat.swift
+++ ReactiveExtensions/operators/Concat.swift
@@ -1,3 +1,4 @@
+// swiftlint:disable line_length
 import ReactiveCocoa
 
 extension SignalType {
@@ -9,35 +10,29 @@ extension SignalType {
 
    - returns: A concatenated signal.
    */
-//  public static func concat<S: SequenceType where S.Generator.Element == Signal<Value, Error>>
-//    (signals: S) -> Signal<Value, Error> {
-//
-//      let producer = SignalProducer<Signal<Value, Error>, Error>(values: signals)
-//      var result: Signal<Value, Error>!
-//      producer.startWithSignal { (signal, _) in
-//        result = signal.flatten(.Concat)
-//      }
-//      return result
-//  }
-//
-//  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-//  public static func concat(s1: Signal<Value, Error>,
-//                            _ s2: Signal<Value, Error>) -> Signal<Value, Error> {
-//
-//    return Signal.concat([s1, s2])
-//  }
-//
-//  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-//  public static func concat(s1: Signal<Value, Error>,
-//                            _ s2: Signal<Value, Error>,
-//                            _ s3: Signal<Value, Error>) -> Signal<Value, Error> {
-//
-//    return Signal.concat([s1, s2, s3])
-//  }
+  public static func concat
+    <Seq: SequenceType, S: SignalType where S.Value == Value, S.Error == Error, Seq.Generator.Element == S>
+    (signals: Seq) -> Signal<Value, Error> {
+
+    let producer = SignalProducer<S, Error>(values: signals)
+    var result: Signal<Value, Error>!
+
+    producer.startWithSignal { signal, _ in
+      result = signal.flatten(.Concat)
+    }
+
+    return result
+  }
+
+  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
+  public static func concat<S: SignalType where S.Value == Value, S.Error == Error>
+    (signals: S...) -> Signal<Value, Error> {
+
+    return Signal.concat(signals)
+  }
 }
 
 extension SignalProducerType {
-
   /**
    Concats a sequence of producers into a single producer.
 
@@ -45,24 +40,18 @@ extension SignalProducerType {
 
    - returns: A concatenated producer.
    */
-//  public static func concat<S: SequenceType where S.Generator.Element == SignalProducer<Value, Error>>
-//    (producers: S) -> SignalProducer<Value, Error> {
-//
-//    return SignalProducer(values: producers).flatten(.Concat)
-//  }
-//
-//  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-//  public static func concat(s1: SignalProducer<Value, Error>,
-//                            _ s2: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
-//
-//    return SignalProducer.concat([s1, s2])
-//  }
-//
-//  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-//  public static func concat(s1: SignalProducer<Value, Error>,
-//                            _ s2: SignalProducer<Value, Error>,
-//                            _ s3: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
-//
-//    return SignalProducer.concat([s1, s2, s3])
-//  }
+  @warn_unused_result(message="Did you forget to call `start` on the producer?")
+  public static func concat
+    <Seq: SequenceType, S: SignalProducerType where S.Value == Value, S.Error == Error, Seq.Generator.Element == S>
+    (producers: Seq) -> SignalProducer<Value, Error> {
+
+    return SignalProducer(values: producers).flatten(.Concat)
+  }
+
+  @warn_unused_result(message="Did you forget to call `start` on the producer?")
+  public static func concat<S: SignalProducerType where S.Value == Value, S.Error == Error>
+    (producers: S...) -> SignalProducer<Value, Error> {
+
+    return SignalProducer.concat(producers)
+  }
 }
