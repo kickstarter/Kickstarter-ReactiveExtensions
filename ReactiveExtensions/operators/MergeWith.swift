24b95a4627d497f1596d3b433b516f3b1f316103
Brandon Williams <mbw234@gmail.com>
2016-06-03 15:21:11 -0400

    Upgrade RAC to 4.2 (#29)
    * Beginning work on upgrading RAC to 4.2
    
    * Removed a bunch of unneeded operators.

--- ReactiveExtensions/operators/MergeWith.swift
+++ ReactiveExtensions/operators/MergeWith.swift
@@ -13,30 +13,6 @@ public extension SignalType {
   public func mergeWith (other: Signal<Value, Error>) -> Signal<Value, Error> {
     return Signal.merge([self.signal, other])
   }
-
-  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-  public static func merge(s1: Signal<Value, Error>,
-                           _ s2: Signal<Value, Error>) -> Signal<Value, Error> {
-
-    return Signal.merge([s1, s2])
-  }
-
-  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-  public static func merge(s1: Signal<Value, Error>,
-                           _ s2: Signal<Value, Error>,
-                           _ s3: Signal<Value, Error>) -> Signal<Value, Error> {
-
-    return Signal.merge([s1, s2, s3])
-  }
-
-  @warn_unused_result(message="Did you forget to call `observe` on the signal?")
-  public static func merge(s1: Signal<Value, Error>,
-                           _ s2: Signal<Value, Error>,
-                           _ s3: Signal<Value, Error>,
-                           _ s4: Signal<Value, Error>) -> Signal<Value, Error> {
-
-    return Signal.merge([s1, s2, s3, s4])
-  }
 }
 
 public extension SignalProducerType {
@@ -51,36 +27,5 @@ public extension SignalProducerType {
   @warn_unused_result(message="Did you forget to call `start` on the producer?")
   public func mergeWith (other: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
     return SignalProducer<SignalProducer<Value, Error>, Error>(values: [self.producer, other]).flatten(.Merge)
-  }
-
-  @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public static func merge <S: SequenceType where S.Generator.Element == SignalProducer<Value, Error>>
-    (s: S) -> SignalProducer<Value, Error> {
-
-    return SignalProducer(values: s).flatten(.Merge)
-  }
-
-  @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public static func merge(p1: SignalProducer<Value, Error>,
-                           _ p2: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
-
-    return SignalProducer(values: [p1, p2]).flatten(.Merge)
-  }
-
-  @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public static func merge(p1: SignalProducer<Value, Error>,
-                           _ p2: SignalProducer<Value, Error>,
-                           _ p3: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
-
-    return SignalProducer(values: [p1, p2, p3]).flatten(.Merge)
-  }
-
-  @warn_unused_result(message="Did you forget to call `start` on the producer?")
-  public static func merge(p1: SignalProducer<Value, Error>,
-                           _ p2: SignalProducer<Value, Error>,
-                           _ p3: SignalProducer<Value, Error>,
-                           _ p4: SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
-
-    return SignalProducer(values: [p1, p2, p3, p4]).flatten(.Merge)
   }
 }
