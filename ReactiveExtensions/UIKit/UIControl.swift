a30b78362287ac8a554daf1a65d8c9f9f6afe5c2
Lisa Luo <lluo92@gmail.com>
2016-06-03 10:33:50 -0400

    Add UISwitch, update UIControl (#30)
    * Add selected property to UIControl for buttons
    
    * add UISwitch
    
    * remove trailing whitespaces
    
    * check for iOS, add tests

--- ReactiveExtensions/UIKit/UIControl.swift
+++ ReactiveExtensions/UIKit/UIControl.swift
@@ -4,14 +4,30 @@ import UIKit
 
 private enum Associations {
   private static var enabled = 0
+  private static var selected = 1
 }
 
 public extension Rac where Object: UIControl {
   public var enabled: Signal<Bool, NoError> {
     nonmutating set {
       let prop: MutableProperty<Bool> = lazyMutableProperty(object, key: &Associations.enabled,
-        setter: { [weak object] in object?.enabled = $0 ?? true },
+        setter: { [weak object] in object?.enabled = $0 },
         getter: { [weak object] in object?.enabled ?? true })
+
+      prop <~ newValue.observeForUI()
+    }
+
+    get {
+      return .empty
+    }
+  }
+
+  public var selected: Signal<Bool, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<Bool> = lazyMutableProperty(
+        object, key: &Associations.selected,
+        setter: { [weak object] in object?.selected = $0 },
+        getter: { [weak object] in object?.selected ?? false })
 
       prop <~ newValue.observeForUI()
     }
---
+++ ReactiveExtensions/UIKit/UISwitch.swift
@@ -0,0 +1,25 @@
+#if os(iOS)
+import ReactiveCocoa
+import Result
+import UIKit
+
+private enum Associations {
+  private static var on = 0
+}
+
+public extension Rac where Object: UISwitch {
+  public var on: Signal<Bool, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<Bool> = lazyMutableProperty(
+        object, key: &Associations.on,
+        setter: { [weak object] in object?.on = $0 },
+        getter: { [weak object] in object?.on ?? false })
+
+      prop <~ newValue.observeForUI()
+    }
+    get {
+      return .empty
+    }
+  }
+}
+#endif
