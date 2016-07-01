2a8924cd7e07bd70359b30dfaf004a8dd7f9c2e8
Brandon Williams <mbw234@gmail.com>
2016-07-01 08:47:44 -0400

    Better equality assertions.

--- ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
+++ ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
@@ -1,37 +1,51 @@
 import XCTest
 import ReactiveCocoa
 
-// Assert equality between two doubly nested arrays of equatables.
-internal func XCTAssertEqual<T: Equatable>(@autoclosure expression1: () -> [[T]],
-  @autoclosure _ expression2: () -> [[T]], _ message: String = "",
-                 file: StaticString = #file, line: UInt = #line) {
+/**
+ Assert equality between two values where an equality operator is provided.
+ */
+internal func XCTAssertEqual <A> (
+  @autoclosure expression1: () -> A,
+               _ equality: (A, A) -> Bool,
+               @autoclosure _ expression2: () -> A,
+                            _ message: String = "",
+                              file: StaticString = #file,
+                              line: UInt = #line) {
 
-    let lhs = expression1()
-    let rhs = expression2()
-    XCTAssertEqual(lhs.count, rhs.count, "Expected \(lhs.count) elements, but found \(rhs.count).",
-      file: file, line: line)
+  let lhs = expression1()
+  let rhs = expression2()
 
-    zip(lhs, rhs).forEach { xs, ys in
-      XCTAssertEqual(xs, ys, "Expected \(lhs), but found \(rhs): \(message)", file: file, line: line)
-    }
+  XCTAssertTrue(equality(lhs, rhs),
+                "Expected \(lhs), but found \(rhs): \(message)",
+                file: file,
+                line: line)
 }
 
-// Assert equality between arrays of optionals of equatables.
-internal func XCTAssertEqual <T: ReactiveCocoa.OptionalType where T.Wrapped: Equatable>
-  (expression1: [T], _ expression2: [T], _ message: String = "",
-   file: StaticString = #file, line: UInt = #line) {
+/**
+ Assert equality between two arrays of values where an equality operator for the values is provided.
+ */
+internal func XCTAssertEqual <A> (
+  @autoclosure expression1: () -> [A],
+               _ equality: (A, A) -> Bool,
+               @autoclosure _ expression2: () -> [A],
+                            _ message: String = "",
+                              file: StaticString = #file,
+                              line: UInt = #line) {
 
-  XCTAssertEqual(
-    expression1.count, expression2.count,
-    "Expected \(expression1.count) elements, but found \(expression2.count).",
-    file: file, line: line
-  )
+  let lhs = expression1()
+  let rhs = expression2()
 
-  zip(expression1, expression2).forEach { xs, ys in
+  XCTAssertEqual(lhs.count, rhs.count,
+                 "Expected \(lhs.count) elements, but found \(rhs.count).",
+                 file: file,
+                 line: line)
+
+  zip(lhs, rhs).forEach { x, y in
     XCTAssertEqual(
-      xs.optional, ys.optional,
-      "Expected \(expression1), but found \(expression2): \(message)",
-      file: file, line: line
+      x, equality, y,
+      "Expected \(lhs), but found \(rhs): \(message)",
+      file: file,
+      line: line
     )
   }
 }
