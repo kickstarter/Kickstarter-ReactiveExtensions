4eacf684e780f40c33981632986c96062067e797
Brandon Williams <mbw234@gmail.com>
2016-07-05 12:55:00 -0400

    Revert "Better equality assertions. (#36)"
    This reverts commit ccc8f7359167c8ee39a471c09a20902207c4abd3.

--- ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
+++ ReactiveExtensionsTests/TestHelpers/XCTAssert-Extensions.swift
@@ -1,45 +1,37 @@
 import XCTest
 import ReactiveCocoa
 
-internal func XCTAssertEqual <A> (
-  @autoclosure expression1: () -> A,
-               _ equality: (A, A) -> Bool,
-               @autoclosure _ expression2: () -> A,
-                            _ message: String = "",
-                              file: StaticString = #file,
-                              line: UInt = #line) {
+// Assert equality between two doubly nested arrays of equatables.
+internal func XCTAssertEqual<T: Equatable>(@autoclosure expression1: () -> [[T]],
+  @autoclosure _ expression2: () -> [[T]], _ message: String = "",
+                 file: StaticString = #file, line: UInt = #line) {
 
-  let lhs = expression1()
-  let rhs = expression2()
+    let lhs = expression1()
+    let rhs = expression2()
+    XCTAssertEqual(lhs.count, rhs.count, "Expected \(lhs.count) elements, but found \(rhs.count).",
+      file: file, line: line)
 
-  XCTAssertTrue(equality(lhs, rhs),
-                "Expected \(lhs), but found \(rhs): \(message)",
-                file: file,
-                line: line)
+    zip(lhs, rhs).forEach { xs, ys in
+      XCTAssertEqual(xs, ys, "Expected \(lhs), but found \(rhs): \(message)", file: file, line: line)
+    }
 }
 
-internal func XCTAssertEqual <A> (
-  @autoclosure expression1: () -> [A],
-               _ equality: (A, A) -> Bool,
-               @autoclosure _ expression2: () -> [A],
-                            _ message: String = "",
-                              file: StaticString = #file,
-                              line: UInt = #line) {
+// Assert equality between arrays of optionals of equatables.
+internal func XCTAssertEqual <T: ReactiveCocoa.OptionalType where T.Wrapped: Equatable>
+  (expression1: [T], _ expression2: [T], _ message: String = "",
+   file: StaticString = #file, line: UInt = #line) {
 
-  let lhs = expression1()
-  let rhs = expression2()
+  XCTAssertEqual(
+    expression1.count, expression2.count,
+    "Expected \(expression1.count) elements, but found \(expression2.count).",
+    file: file, line: line
+  )
 
-  XCTAssertEqual(lhs.count, rhs.count,
-                 "Expected \(lhs.count) elements, but found \(rhs.count).",
-                 file: file,
-                 line: line)
-
-  zip(lhs, rhs).forEach { x, y in
+  zip(expression1, expression2).forEach { xs, ys in
     XCTAssertEqual(
-      x, equality, y,
-      "Expected \(lhs), but found \(rhs): \(message)",
-      file: file,
-      line: line
+      xs.optional, ys.optional,
+      "Expected \(expression1), but found \(expression2): \(message)",
+      file: file, line: line
     )
   }
 }
