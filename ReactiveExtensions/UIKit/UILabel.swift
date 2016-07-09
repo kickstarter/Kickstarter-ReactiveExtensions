159a94e9b07869a3ddee62e74e5bfbc755ecfed8
Brandon Williams <mbw234@gmail.com>
2016-07-09 13:31:21 -0400

    Some reactive bindings for attributed strings.

--- ReactiveExtensions/UIKit/UILabel.swift
+++ ReactiveExtensions/UIKit/UILabel.swift
@@ -3,12 +3,29 @@ import Result
 import UIKit
 
 private enum Associations {
-  private static var text = 0
+  private static var attributedText = 0
   private static var font = 1
-  private static var textColor = 2
+  private static var text = 2
+  private static var textColor = 3
 }
 
 public extension Rac where Object: UILabel {
+  public var attributedText: Signal<NSAttributedString, NoError> {
+    nonmutating set {
+      let prop: MutableProperty<NSAttributedString> = lazyMutableProperty(
+        object,
+        key: &Associations.attributedText,
+        setter: { [weak object] in object?.attributedText = $0 },
+        getter: { [weak object] in object?.attributedText ?? .init(string: "") })
+
+      prop <~ newValue.observeForUI()
+    }
+
+    get {
+      return .empty
+    }
+  }
+
   public var text: Signal<String, NoError> {
     nonmutating set {
       let prop: MutableProperty<String> = lazyMutableProperty(
