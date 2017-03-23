2b91080cab5e3a8876cea81a30665f6605bc4edd
Justin Swart <jwswart@gmail.com>
2017-03-23 13:00:17 +0200

    Add rac extension for attributedPlaceholder (#59)
    * Add rac extension for attributedPlaceholder
    
    * Add new Associations key

--- ReactiveExtensions/UIKit/UITextFieldTests.swift
+++ ReactiveExtensions/UIKit/UITextFieldTests.swift
@@ -8,6 +8,17 @@ import UIKit
 final class UITextFieldTests: XCTestCase {
   let textField = UITextField()
 
+  func testAttributedPlaceHolder() {
+    let (signal, observer) = Signal<NSAttributedString, NoError>.pipe()
+    textField.rac.attributedPlaceholder = signal
+
+    observer.send(value: NSAttributedString(string: "The future"))
+    eventually(XCTAssertEqual("The future", self.textField.attributedPlaceholder?.string))
+
+    observer.send(value: NSAttributedString(string: ""))
+    eventually(XCTAssertEqual("", self.textField.attributedPlaceholder?.string))
+  }
+
   func testText() {
     let (signal, observer) = Signal<String, NoError>.pipe()
     textField.rac.text = signal
