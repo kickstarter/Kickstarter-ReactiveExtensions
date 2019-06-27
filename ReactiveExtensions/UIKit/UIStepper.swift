aa994f0921dca1965ae537fe77c2edc798eef230
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-06-27 14:47:02 -0700

    Add stepValue rac extension (#77)

--- ReactiveExtensions/UIKit/UIStepper.swift
+++ ReactiveExtensions/UIKit/UIStepper.swift
@@ -2,18 +2,19 @@ import ReactiveSwift
 import UIKit
 
 private enum Associations {
-  fileprivate static var value = 0
-  fileprivate static var minimumValue = 0
   fileprivate static var maximumValue = 0
+  fileprivate static var minimumValue = 0
+  fileprivate static var stepValue = 0
+  fileprivate static var value = 0
 }
 
 public extension Rac where Object: UIStepper {
-  var value: Signal<Double, Never> {
+  var maximumValue: Signal<Double, Never> {
     nonmutating set {
       let prop: MutableProperty<Double> = lazyMutableProperty(
-        object, key: &Associations.value,
-        setter: { [weak object] in object?.value = $0 },
-        getter: { [weak object] in object?.value ?? 0.0 })
+        object, key: &Associations.maximumValue,
+        setter: { [weak object] in object?.maximumValue = $0 },
+        getter: { [weak object] in object?.maximumValue ?? 100.0 })
 
       prop <~ newValue.observeForUI()
     }
@@ -36,12 +37,26 @@ public extension Rac where Object: UIStepper {
     }
   }
 
-  var maximumValue: Signal<Double, Never> {
+  var stepValue: Signal<Double, Never> {
     nonmutating set {
       let prop: MutableProperty<Double> = lazyMutableProperty(
-        object, key: &Associations.maximumValue,
-        setter: { [weak object] in object?.maximumValue = $0 },
-        getter: { [weak object] in object?.maximumValue ?? 100.0 })
+        object, key: &Associations.stepValue,
+        setter: { [weak object] in object?.stepValue = ($0 > 0 ? $0 : 1.0 ) },
+        getter: { [weak object] in object?.stepValue ?? 1.0 })
+
+      prop <~ newValue.observeForUI()
+    }
+    get {
+      return .empty
+    }
+  }
+
+  var value: Signal<Double, Never> {
+    nonmutating set {
+      let prop: MutableProperty<Double> = lazyMutableProperty(
+        object, key: &Associations.value,
+        setter: { [weak object] in object?.value = $0 },
+        getter: { [weak object] in object?.value ?? 0.0 })
 
       prop <~ newValue.observeForUI()
     }
