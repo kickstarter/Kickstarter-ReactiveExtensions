0631d6c730290f41983cebf5bf604b64ea6f9a2e
Mubarak Sadoon <msadoon@gmail.com>
2022-10-12 16:10:02 -0400

    Updated target names to match library names.
    This might fix an issue when this framework is imported of not being able to import ReactiveExtensions. Previously ReactiveExtensions was the library name but not the target name.

--- Package.swift
+++ Package.swift
@@ -6,23 +6,23 @@ let package = Package(
   products: [
     .library(
       name: "ReactiveExtensions",
-      targets: ["ReactiveExtensions-iOS"]
+      targets: ["ReactiveExtensions"]
     ),
     .library(
       name: "ReactiveExtensions-TestHelpers",
-      targets: ["ReactiveExtensions-TestHelpers-iOS"]
+      targets: ["ReactiveExtensions-TestHelpers"]
     )
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", from: "6.5.0")
       ],
   targets: [
-    .target(name: "ReactiveExtensions-iOS", dependencies: ["ReactiveSwift"]),
+    .target(name: "ReactiveExtensions", dependencies: ["ReactiveSwift"]),
     .testTarget(
       name: "ReactiveExtensions-iOSTests",
-      dependencies: ["ReactiveExtensions-iOS", "ReactiveExtensions-TestHelpers-iOS"],
+      dependencies: ["ReactiveExtensions", "ReactiveExtensions-TestHelpers"],
       path: "Tests"
     ),
-    .target(name: "ReactiveExtensions-TestHelpers-iOS", dependencies: ["ReactiveSwift"])
+    .target(name: "ReactiveExtensions-TestHelpers", dependencies: ["ReactiveSwift"])
   ]
 )
