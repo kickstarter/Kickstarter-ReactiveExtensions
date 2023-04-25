f0687eee72e674deb24aa3483f0032b0ae81a91c
Mubarak Sadoon <msadoon@gmail.com>
2023-04-25 10:45:12 -0400

    updated package manifest to include tvOS because its now compiling and tests are running against it.

--- Package.swift
+++ Package.swift
@@ -11,10 +11,19 @@ let package = Package(
     .library(
       name: "ReactiveExtensions-TestHelpers",
       targets: ["ReactiveExtensions-TestHelpers"]
+    ),
+    .library(
+      name: "ReactiveExtensions-tvOS",
+      targets: ["ReactiveExtensions-tvOS"]
+    ),
+    .library(
+      name: "ReactiveExtensions-TestHelpers-tvOS",
+      targets: ["ReactiveExtensions-TestHelpers-tvOS"]
     )
   ],
   dependencies: [
-    .package(name: "ReactiveSwift", url: "https://github.com/ReactiveCocoa/ReactiveSwift", .exact("6.5.0")),
+    .package(name: "ReactiveSwift", url: "https://github.com/ReactiveCocoa/ReactiveSwift", .exact("7.1.1"))
+    ],
   targets: [
     .target(name: "ReactiveExtensions", dependencies: ["ReactiveSwift"],
             linkerSettings: [
@@ -26,6 +35,19 @@ let package = Package(
       path: "Tests"
     ),
     .target(name: "ReactiveExtensions-TestHelpers", dependencies: ["ReactiveSwift"],
+            linkerSettings: [
+                .linkedFramework("Foundation")
+            ]),
+    .target(name: "ReactiveExtensions-tvOS", dependencies: ["ReactiveSwift"],
+            linkerSettings: [
+                .linkedFramework("Foundation")
+            ]),
+    .testTarget(
+      name: "ReactiveExtensions-tvOSTests",
+      dependencies: ["ReactiveExtensions-tvOS", "ReactiveExtensions-TestHelpers-tvOS"],
+      path: "Tests"
+    ),
+    .target(name: "ReactiveExtensions-TestHelpers-tvOS", dependencies: ["ReactiveSwift"],
             linkerSettings: [
                 .linkedFramework("Foundation")
             ])
